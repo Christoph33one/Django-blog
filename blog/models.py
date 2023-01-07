ef1b44f042c468860c80a6d86351d302036d00af
Christoph33one <cjbrowning23@hotmail.com>
2023-01-07 10:47:28 +0000

    started building models.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+STATS = ((0, "Draft"), (1,"Published"))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    # Adding a character field for max 200 characters
