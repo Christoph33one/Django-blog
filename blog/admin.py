083a28c0cf1f0f5b72fa4814b8d86bfa54b363f4
Christoph33one <cjbrowning23@hotmail.com>
2023-01-10 11:32:47 +0000

    starting adding views. created code in views.py file. Also createdindex and base.html in templates folder

--- blog/admin.py
+++ blog/admin.py
@@ -9,8 +9,11 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
-    # gives a prediction on what the user will add to a title or content box
     summernote_fields = ('content')
+    # gives a prediction on what the user will add to a title or content box (hello world)
+    # look when typing in the title box, text follows in slug catagory also. (hello world)
+    # this forms part or the URl. example:www.codestart2021.heroky.com/helloworld
+    # list_filter gives the user a by filter method, time, date or day
 
 
 @admin.register(Comment)
@@ -26,6 +29,6 @@ class CommemntAdmin(admin.ModelAdmin):
 
     def approved_comments(self, request, queryset):
         queryset.update(approved=True)
-        # queryset update. 
+        # queryset update.
         # updated to approved is True
-        # Commemts can now be approved!
+        # Commemts can now be approved by admin user
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
+
 
 # Create your views here.
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = 'index.html' # html file to be rendered
+    paginate_by = 6 
+    # means it will only show 6 posts per page, 
+    # and create new pages for more posts.
+    # this can be any number you like.
+    
