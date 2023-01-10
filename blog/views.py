1ff521283fec23a15532646a2b85067567e1138f
Christoph33one <cjbrowning23@hotmail.com>
2023-01-10 12:21:27 +0000

    added template in index.html

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,12 @@ from django.views import generic
 from .models import Post
 
 
-# Create your views here.
+# view created. step 1
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = 'index.html' # html file to be rendered
-    paginate_by = 6 
-    # means it will only show 6 posts per page, 
+    template_name = 'index.html' #  html file to be rendered
+    paginate_by = 6
+    # means it will only show 6 posts per page,
     # and create new pages for more posts.
     # this can be any number you like.
-    
