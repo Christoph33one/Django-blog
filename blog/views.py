41a1dfa1c16b62d645dbaa5a42190de9c0e6d6cc
Christoph33one <cjbrowning23@hotmail.com>
2023-01-16 18:27:34 +0000

    added commet functina

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
 # view created. step 1 (add a view)
@@ -20,9 +21,11 @@ class PostDetail(View):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = False  # if user does not like post, post is always False
+        liked = False
+        # if user does not like post, post is always False
         if post.likes.filter(id=self.request.user.id).exists():
-            liked = True  # if user likes post, post is set to True
+            liked = True
+            # if user likes post, post is set to True
 
         return render(
             request,
@@ -30,6 +33,48 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
+                "commented": False,
+                # User comment is False until approved by admin
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        # if user likes post, post is set to True
+
+        comment_form = CommentForm(data=request.POST)
+        # getting the data from the form.
+
+        # If Comment form is valid (all form fields are completed)
+        # This below is to process the Comment form.
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+            # else return an empty comment form if mot valid!
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                # commented = true, tells the user
+                # their comment is waiting approval!
+                "comment_form": comment_form,
                 "liked": liked
             },
         )
